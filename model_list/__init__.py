c34d2ed98b0b5ca4e5b9849b6fd9639fcd188c99
wxxu <xuweixiang1996@foxmail.com>
2022-04-04 13:35:59 +0800

    Create __init__.py

---
+++ model_list/__init__.py
@@ -0,0 +1,4 @@
+from .resnet import resnet50
+
+from .resnet18_bwx2 import resnet18_bwx2 # with two parallel binary weights
+from .resnet18_ter import resnet18_ter # with the equivalent ternary weights
